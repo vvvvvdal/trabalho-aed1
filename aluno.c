e3a2622a7342f96f46083167be5b3b0fcade11b5
felipe vidal <136502385+vvvvvdal@users.noreply.github.com>
2026-01-30 17:21:01 -0300

    inicialização do projeto
    na main: somente inicialização com bibliotecas stdio.h, stdlib.h e string.h
    
    na aluno.c: inicialização com bibliotecas stdio.h, stdlib.h e string.h. implementação da função limpar_buffer_entrada(), que limpa a entrada de dados do usuario, para evitar algum erro nessa entrada.
    
    aluno.h: implementação das definições de constantes, estruturas e a maior parte das funções.

---
+++ aluno.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void limpar_buffer_entrada() {
+    int c;
+    while((c = getchar()) != '\n' && c != EOF);
+}
+
+int main() {
+
+    return 0;
+}
---
+++ aluno.h
@@ -0,0 +1,70 @@
+#ifndef ALUNO_H
+#define ALUNO_H
+
+#define MAX_ALUNOS 50
+#define MAX_ESPERA 50
+#define MAX_HISTORICO 50
+
+#define ID_NULA -1
+#define TAM_NULO -1
+
+typedef struct {
+    int id;
+    int idade;
+    char nome[128];
+} Aluno; // elemento
+
+typedef struct {
+    Aluno alunos[MAX_ALUNOS];
+    int tam_lista;
+} Lista_turma; // lista estática
+
+typedef struct {
+    Aluno alunos[MAX_ESPERA];
+    int tam_fila;
+    int inicio_fila;
+    int fim_fila;
+} Fila_espera; // fila estática
+
+typedef struct {
+    Aluno alunos[MAX_HISTORICO];
+    int tam_pilha;
+} Pilha_historico; // pilha estática
+
+void limpar_buffer_entrada();
+int verificar_id(int id);
+int verificar_idade(int idade);
+int verificar_nome(char *nome);
+Aluno buscar_aluno(Lista_turma turma, int id);
+
+// funções de criar
+int criar_lista_turma(Lista_turma *turma);
+int criar_fila_espera(Fila_espera *turma_espera);
+int criar_pilha_historico(Pilha_historico *turma_historico);
+
+// funções de imprimir
+void print_aluno(Aluno aluno);
+void print_lista_turma(Lista_turma turma);
+void print_fila_espera(Fila_espera turma_espera);
+void print_pilha_historico(Pilha_historico turma_historico);
+
+// lista: inserir (push) e remover (pop)
+int push_lista_turma(Lista_turma *turma, int id_aluno, int idade_aluno, char *nome_aluno);
+Aluno pop_lista_turma(Lista_turma *turma, int id_aluno);
+
+// fila: inserir (push) e remover (pop)
+int push_fila_espera(Fila_espera *turma_espera, Aluno aluno);
+Aluno pop_fila_espera(Fila_espera *turma_espera);
+
+// pilha: inserir (push) e remover (pop)
+int push_pilha_historico(Pilha_historico *turma_historico, Aluno aluno);
+Aluno pop_pilha_historico(Pilha_historico *turma_historico);
+
+
+// recuperar um aluno que foi apagado
+int recuperar_historico_aluno(Lista_turma *turma, Pilha_historico *historico);
+
+// colocar um aluno da lista de espera para dentro da turma
+int sair_espera_aluno(Lista_turma *turma, Fila_espera *espera);
+
+#endif
